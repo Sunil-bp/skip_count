0e2390fcbddef0dc7df58bfe28e591c6f65ac781
Your Name <sunilbpoojari@gmail.com>
2020-09-18 13:18:36 +0530

    Added conture around skin detected

--- skip_count.py
+++ skip_count.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import math
+import traceback
+import sys
+
 
 #read from primary camera
 cap = cv2.VideoCapture(0)
@@ -23,16 +26,17 @@ while (1):
         frame = cv2.flip(frame, 1)
         kernel = np.ones((3, 3), np.uint8)
         # define region of interest
-        roi = frame[0:200, 200:480]
+        roi = frame[200:480, 0:640]
 
         hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
         cv2.rectangle(frame, (0, 200), (640,480), (0, 255, 0), 3)
         cv2.line(frame, (0, 0), (640, 480), (255, 0, 0), 1)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        count = 0
+        cv2.putText(frame, str(count), (450, 80), font, 3, (255, 255, 255), 1, cv2.LINE_AA)
         # define range of skin color in HSV
-        lower_skin = np.array([0, 20, 70], dtype=np.uint8)
+        lower_skin = np.array([0, 44, 71], dtype=np.uint8)
         upper_skin = np.array([20, 255, 255], dtype=np.uint8)
 
         # extract skin colur imagw
@@ -45,6 +49,19 @@ while (1):
         mask = cv2.GaussianBlur(mask, (5, 5), 100)
 
         # find contours
+        # find contours
+        _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # find contour of max area(hand)
+        cnt = max(contours, key=lambda x: cv2.contourArea(x))
+
+        # approx the contour a little
+        epsilon = 0.0005 * cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, epsilon, True)
+
+        # make convex hull around hand
+        hull = cv2.convexHull(cnt)
+
         cv2.imshow('mask', mask)
         cv2.imshow('frame', frame)
     except Exception as e:
